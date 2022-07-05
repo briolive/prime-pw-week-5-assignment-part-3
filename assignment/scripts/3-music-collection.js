61ebd352c1627e971a1e33ad2f26f9ddec2ccb95
Brianna Oliveira <mystikgab@gmail.com>
2022-07-05 13:30:43 -0500

    completed addToCollection task

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,12 +4,28 @@ let collection = []
 
 
 /**
- * @param title
- * @param artist
- * @param yearPublished
+ * @param {string, string, number} title, artist, yearPublished
  * @return new object with the above properties, added to the end of the collection array
  */
 
-function addToCollection(){
-    
+function addToCollection(title, artist, yearPublished){
+    console.log('in addToCollection');
+    const newObject = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    console.log('new album added:', newObject);
+    collection.push(newObject);
+    console.log('updated collection:', collection)
+    return newObject;
 }
+
+addToCollection('Ctrl', 'Sza', 2017);
+addToCollection('Z', 'Sza', 2014);
+addToCollection('Punisher', 'Phoebe Bridgers', 2020);
+addToCollection('Farm to Table', 'Bartees Strange', 2022);
+addToCollection('Lush', 'Snail Mail', 2018);
+addToCollection('Impossible Weight', 'Deep Sea Diver', 2020);
+
+console.log(collection);
